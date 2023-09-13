848115f42bc4adb9eb12ebae3e5d0b4ac117f720
lsmescolotto <lsmescolotto@gmail.com>
2023-09-13 18:11:57 -0300

    feat: basic functional test to a button with rtl

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+// eslint-disable-next-line testing-library/no-dom-import
+import { logRoles } from "@testing-library/dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color and updates when clicked', () => {
+  const {container} = render(<App/>);
+  logRoles(container)
+  //getting element in dom
+  const colorButton = screen.getByRole("button", {name: "Change to blue"})
+  //test if the button is initially red
+  expect(colorButton).toHaveStyle("background-color: red")
+
+  //test if after clicked, the button turns blue
+  fireEvent.click(colorButton)
+  expect(colorButton).toHaveStyle("background-color: blue")
+
+  //test if the button text is "Change to red"
+  expect(colorButton).toHaveTextContent("Change to red")
 });
+
